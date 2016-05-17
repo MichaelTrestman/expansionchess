113182bd60d6a9a033621e01a83ec8c67bc64690
MichaelTrestman <mtrestman@pivotal.io>
2016-05-17 12:13:09 -0700

    Pieces displace previous occupant of space when placed.

--- js/boardfunctions/placePiece.js
+++ js/boardfunctions/placePiece.js
@@ -1,82 +1,54 @@
 BoardFunctions.placePiece = function(type, side, $square){
 
-  // var piece = $("<div class='piece side-" + side + " piece-" + type + "' data-piece-type='"+type+"' data 'draggable='true' ondragstart='drag(event)' ></div>");
-  console.log('$square[0].dataset')
-  console.log($square[0].dataset)
+  var piece = $("<div></div>")
+    .addClass('piece')
+    .addClass('piece-' + type)
+    .addClass('side-' + side)
+    .attr('draggable', 'true')
+    .attr('data-type', type)
+    .attr('data-side', side)
+    .attr('data-posX', $square[0].dataset.posx)
+    .attr('data-posY', $square[0].dataset.posy)
+    .on('dragstart', drag);
 
-  var piece = $("<div></div>");
-  piece.addClass('piece')
-  piece.addClass('piece-' + type)
-  piece.addClass('side-' + side)
-  piece.attr('draggable', 'true')
-  piece.attr('data-type', type)
-  piece.attr('data-side', side)
-  piece.attr('data-posX', $square[0].dataset.posx)
-  piece.attr('data-posY', $square[0].dataset.posy)
-  piece.on('dragstart', drag)
+  if ($square.hasClass('square')) {
+    $square.children('.piece').remove();
+  } else if ($square.hasClass('pieces-picker')){
+
+  } else {
+    throw 'Target is not a square!!!'
+  };
 
   $square.append(piece);
-
-// piece[0].addEventListener('dragstart', PieceFunctions.handlePiecePickup, false);
-
-// piece[0].addEventListener('drop', PieceFunctions.handlePieceDrop, false);
-
-
 }
 
 
 function allowDrop(ev) {
 
+  // if ($(ev.target).hasClass('square')) {
     ev.preventDefault();
-    console.log('allowing drop!')
+  console.log('allowing drop!')
+  // };
+
 }
 
 function drag(ev) {
-    // ev.originalEvent.dataTransfer.setData("text", ev.target.id);
     var pieceData = ev.target.dataset;
 
-    console.log("pieceData.side")
-    console.log(pieceData.side)
-    ev.originalEvent.dataTransfer.setData('piece-side', pieceData.side)
-
-    console.log('type')
-    console.log(pieceData.type)
-    ev.originalEvent.dataTransfer.setData('piece-type', pieceData.type)
-
-    console.log('posx')
-    console.log(pieceData.posx)
-    ev.originalEvent.dataTransfer.setData('piece-posX', pieceData.posx)
-
-
-
-    console.log('posy')
-    console.log(pieceData.posy)
-    ev.originalEvent.dataTransfer.setData('piece-posY', pieceData.posy)
-
+    ev.originalEvent.dataTransfer.setData('piece-side', pieceData.side);
+    ev.originalEvent.dataTransfer.setData('piece-type', pieceData.type);
+    ev.originalEvent.dataTransfer.setData('piece-posX', pieceData.posx);
+    ev.originalEvent.dataTransfer.setData('piece-posY', pieceData.posy);
 }
 
 function drop(ev) {
     ev.preventDefault();
-    console.log('drop it like its hot')
-    console.log(ev)
-    // console.log(ev.originalEvent.dataTransfer.getData('obj'))
 
-    console.log('original piece was a:')
-    console.log("ev.originalEvent.dataTransfer.getData('piece-side')")
-    console.log(ev.originalEvent.dataTransfer.getData('piece-side'))
     var side = ev.originalEvent.dataTransfer.getData('piece-side');
 
-    console.log("ev.originalEvent.dataTransfer.getData('piece-type')")
-    console.log(ev.originalEvent.dataTransfer.getData('piece-type'))
     var type = ev.originalEvent.dataTransfer.getData('piece-type');
 
-    BoardFunctions.placePiece(type, side, $(ev.target))
 
-    console.log("ev.originalEvent.dataTransfer.getData('piece-posX')");
-    console.log(ev.originalEvent.dataTransfer.getData('piece-posX'));
-
-    console.log("ev.originalEvent.dataTransfer.getData('piece-posY')");
-    console.log(ev.originalEvent.dataTransfer.getData('piece-posY'));
 
     var oldPiecePosX = ev.originalEvent.dataTransfer.getData('piece-posX');
     var oldPiecePosY = ev.originalEvent.dataTransfer.getData('piece-posY');
@@ -84,29 +56,19 @@ function drop(ev) {
     var oldPieceSelector = '.piece';
     oldPieceSelector += '[data-posX="' +  oldPiecePosX + '"]';
     oldPieceSelector += '[data-posY="' +  oldPiecePosY + '"]';
-    console.log("oldPieceSelector");
-    console.log(oldPieceSelector);
+
     $(oldPieceSelector).remove();
 
+    var $target = $(ev.target);
 
+    if ($target.hasClass('piece')) {
+      $target = $target.parent()
+
+    }
+
+    if (!$target.hasClass('square')) {
+      throw 'Target is not square!'
+    };
+
+    BoardFunctions.placePiece(type, side, $target)
 }
-
-
-
-
-
-// PieceFunctions.handlePiecePickup = function(e){
-//   console.log('piece picked up!')
-//   var imgUrl = $(e.target).css('background-image')
-//   var img = document.createElement("img");
-//   imgUrl = imgUrl.split('"')[1];
-//   img.src = imgUrl;
-//   e.dataTransfer.setDragImage(img, 0,0)
-
-//   e.dataTransfer.effectAllowed = 'move';
-
-// }
-
-// PieceFunctions.handlePieceDrop = function(e){
-//   console.log('piece dropped!')
-// }
